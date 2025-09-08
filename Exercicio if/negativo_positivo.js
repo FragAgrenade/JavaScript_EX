32e5bbcf71ced9404fec1886f5c0cb95dfd27d63
Matheus Fraga Torquato <fragamatheus840@gmail.com>
2025-09-08 09:47:10 -0300

    Atualização 08/09/25 09:47

---
+++ Exercicio if/negativo_positivo.js
@@ -0,0 +1,23 @@
+{
+    const num = -2
+
+    if (num >= 0) {
+        console.log('Positivo')
+    } else {
+        console.log('Negativo')
+    }
+}
+
+/* função */
+{
+    function verificarNumero(num) {
+        if (num >= 0) {
+            console.log('Positivo')
+        } else {
+            console.log('Negativo')
+        }
+    }
+
+verificarNumero(10)
+verificarNumero(-30)
+}
